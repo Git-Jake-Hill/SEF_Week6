08cd66554bcc34ca1d308e6615a2de1c570c9788
Git-Jake-Hill <89761486+Git-Jake-Hill@users.noreply.github.com>
2023-04-06 15:34:49 +1000

    Implementation of shape

--- src/ShapeSpecialisation/Shape.java
+++ src/ShapeSpecialisation/Shape.java
@@ -1,5 +1,84 @@
 package ShapeSpecialisation;
 
-abstract class Shape {
+import java.util.List;
 
+class Graphics {
+    private List<Integer> x;
+    private List<Integer> y;
+    private Shape shape;
+
+    public Graphics(Shape shape) {
+        this.shape = shape;
+    }
+
+    public void addCoordX(Integer x1) {
+        x.add(x1);
+    }
+
+    public void addCoordY(Integer y1) {
+        y.add(y1);
+    }
+}
+
+abstract class Shape {
+    private Graphics g;
+
+    public void draw() {
+        // Draw shape
+
+    };
+
+    public double getArea() {
+        double area = g.x * g.y;
+        return area;
+    };
+
+    public double getBoundingArea() {
+        double area = g.x * g.y;
+        return area;
+    };
+
+}
+
+class Square extends Shape {
+    public void draw() {
+        // draw square
+    };
+
+    public double getArea() {
+        double area = x * y;
+        return area;
+    };
+
+    public double getBoundingArea() {
+        return 0.0;
+    };
+
+    public boolean checkEqualSides() {
+        return true;
+    }
+}
+
+class Circle extends Shape {
+    private double radious;
+
+    public void draw() {
+        // draw square
+    };
+
+    public double getArea() {
+        return radious * 3.14;
+    };
+
+    public double getBoundingArea() {
+        return 0.0;
+    };
+
+    public double getRadious() {
+        return 0.0;
+    }
+
+    public double getPreimeter() {
+        return 0.0;
+    }
 }
